99b1cd3c3359690ab2c927a84e25e02205c5a1d7
Jade <jadeyyuu@gmail.com>
2019-07-14 22:08:52 -0500

    Added views to create/edit blog post inside the site.

--- DjangoBlog/mysite/mysite/settings.py
+++ DjangoBlog/mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 're%n@u!l0@a)-02rf8%pz06@77-e=(h-q#o3h-a2(_)glm7)6k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
